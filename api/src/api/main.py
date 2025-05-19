ae9eba3d4c775895898101d155b2767da32ed21a
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2025-05-19 10:15:37 +0200

    Securitize Swagger

--- api/src/api/main.py
+++ api/src/api/main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 
 from api.dependency_container import DependencyContainer
 from api.workflows.products import product_router
+from common.application_environment import ApplicationEnvironment
 
 DependencyContainer.initialize()
-app = FastAPI()
+openapi_url = (
+    "/openapi.json"
+    if ApplicationEnvironment.get_current() != ApplicationEnvironment.PRODUCTION
+    else None
+)
+app = FastAPI(title="Python monorepo", version="0.1.0", openapi_url=openapi_url)
 app.include_router(product_router.router)
-
-
-@app.get("/")
-async def root() -> str:
-    return "Hello"
