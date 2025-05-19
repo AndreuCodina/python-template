b7ba96a623ff2b8b0a791dbd2502ef7aaa15439b
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2025-05-19 10:24:53 +0200

    Add telemetry

--- api/src/api/main.py
+++ api/src/api/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from opentelemetry.instrumentation.fastapi import (  # type: ignore[reportMissingTypeStubs]
+    FastAPIInstrumentor,
+)
 
 from api.dependency_container import DependencyContainer
 from api.workflows.products import product_router
 from common.application_environment import ApplicationEnvironment
+
+
+def add_telemetry(app: FastAPI) -> None:
+    FastAPIInstrumentor.instrument_app(app)  # type: ignore[reportUnknownMemberType]
+
 
 DependencyContainer.initialize()
 openapi_url = (
@@ -11,4 +20,11 @@ openapi_url = (
     else None
 )
 app = FastAPI(title="Python monorepo", version="0.1.0", openapi_url=openapi_url)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(product_router.router)
