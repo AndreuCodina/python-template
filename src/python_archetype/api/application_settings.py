31ece921ccec2d7e75e09f9d2a986620b31cb92a
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2025-09-17 13:22:57 +0200

    Simplify ApplicationSettings

--- src/python_archetype/api/application_settings.py
+++ src/python_archetype/api/application_settings.py
@@ -1,26 +1,32 @@
 from pathlib import Path
 
 from azure.identity import DefaultAzureCredential
-from pydantic import SecretStr
-from pydantic.alias_generators import to_pascal
+from pydantic import Field, SecretStr
 from pydantic_settings import (
     AzureKeyVaultSettingsSource,
     BaseSettings,
-    DotEnvSettingsSource,
-    EnvSettingsSource,
     PydanticBaseSettingsSource,
     SettingsConfigDict,
 )
 
 from python_archetype.common.application_environment import ApplicationEnvironment
 
+current_path = Path(__file__).parent.resolve()
+
 
 class ApplicationSettings(BaseSettings):
-    model_config = SettingsConfigDict(alias_generator=to_pascal, extra="ignore")
+    model_config = SettingsConfigDict(
+        extra="ignore",
+        env_nested_delimiter="__",
+        env_file=(
+            str(current_path / ".env"),
+            str(current_path / f".env.{ApplicationEnvironment.get_current()}"),
+        ),
+    )
 
     logging_level: str
     cosmos_db_no_sql_url: str
-    cosmos_db_no_sql_key: SecretStr
+    cosmos_db_no_sql_key: SecretStr = Field(alias="CosmosDbNoSqlKey")
     cosmos_db_no_sql_database: str
 
     @classmethod
@@ -28,32 +34,18 @@ class ApplicationSettings(BaseSettings):
         cls,
         settings_cls: type[BaseSettings],
         init_settings: PydanticBaseSettingsSource,  # noqa: ARG003
-        env_settings: PydanticBaseSettingsSource,  # noqa: ARG003
-        dotenv_settings: PydanticBaseSettingsSource,  # noqa: ARG003
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
         file_secret_settings: PydanticBaseSettingsSource,  # noqa: ARG003
     ) -> tuple[PydanticBaseSettingsSource, ...]:
-        env = EnvSettingsSource(
-            settings_cls,
-            env_nested_delimiter="__",
-        )
-        current_path = Path(__file__).parent.resolve()
-        dotenv = DotEnvSettingsSource(
-            settings_cls,
-            env_file=[
-                str(current_path / ".env"),
-                str(current_path / f".env.{ApplicationEnvironment.get_current()}"),
-            ],
-            env_nested_delimiter=":",
-            case_sensitive=True,
-        )
-        settings = (env, dotenv)
+        settings = (env_settings, dotenv_settings)
 
         if ApplicationEnvironment.get_current() != ApplicationEnvironment.LOCAL:
             azure_key_vault = AzureKeyVaultSettingsSource(
                 settings_cls,
-                dotenv()["AzureKeyVaultUrl"],
+                dotenv_settings()["azure_key_vault_url"],
                 DefaultAzureCredential(),
             )
-            settings += (azure_key_vault,)
+            settings = (azure_key_vault, *settings)
 
         return settings
