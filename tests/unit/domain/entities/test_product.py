e188aabe636f07ced16884446663927ddfb0fb38
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2025-09-29 06:04:19 +0200

    Rename

---
+++ tests/unit/domain/entities/test_product.py
@@ -0,0 +1,19 @@
+import pytest
+from test_utils.builders.domain.entities.product_builder import ProductBuilder
+
+from python_template.common.business_error import (
+    ProductAlreadyDiscontinuedError,
+)
+
+
+class TestProduct:
+    def test_discontinue_product(self) -> None:
+        product = ProductBuilder().build()
+
+        product.discontinue()
+
+    def test_fail_when_discontinuing_discontinued_product(self) -> None:
+        product = ProductBuilder().discontinued().build()
+
+        with pytest.raises(ProductAlreadyDiscontinuedError):
+            product.discontinue()
