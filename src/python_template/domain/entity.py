faf97f47899dcb846623977df774aa5f4a3c60b7
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2025-10-08 08:02:04 +0200

    Update packages

--- src/python_template/domain/entity.py
+++ src/python_template/domain/entity.py
@@ -25,6 +25,7 @@ class Entity(BaseModel):
         exclude_unset: bool = False,
         exclude_defaults: bool = False,
         exclude_none: bool = False,
+        exclude_computed_fields: bool = False,
         round_trip: bool = False,
         warnings: bool | Literal["none", "warn", "error"] = True,
         fallback: Callable[[Any], Any] | None = None,
