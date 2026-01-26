37a5c1134886d679846d386bf0546d2839a6427e
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2026-01-26 21:01:58 +0100

    Organize main

--- tests/integration/api/conftest.py
+++ tests/integration/api/conftest.py
@@ -3,12 +3,10 @@ from collections.abc import AsyncGenerator
 import pytest
 from aspy_dependency_injection.service_provider import ServiceProvider
 
-from python_template.api.main import configure_services
+from python_template.api.main import services
 
 
 @pytest.fixture
 async def service_provider() -> AsyncGenerator[ServiceProvider]:
-    services = configure_services()
-
     async with services.build_service_provider() as service_provider:
         yield service_provider
